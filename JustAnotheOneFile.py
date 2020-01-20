9c33cc5fdb5e56960d324d383af89fb11c07197b
GhostOfCode <ghostofdeath2023@gmail.com>
2020-01-20 03:32:33 +0300

    A 2nd commit

--- JustAnotheOneFile.py
+++ JustAnotheOneFile.py
@@ -1,3 +1,4 @@
 #So, that's just another one file just in a study case, no more no less
 print('Hello Git!\nHello GitHub!')
 print("And now let's start a commiting process!")
+print("And a 2nd commit?")
