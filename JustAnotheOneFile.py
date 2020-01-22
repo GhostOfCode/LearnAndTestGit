33e0d849656de68c3d4517875f3ebfaa676eb553
GhostOfCode <ghostofdeath2023@gmail.com>
2020-01-23 00:33:03 +0300

    Test a branches - so, a files saving in a rep-s and no they are not still be connected

--- JustAnotheOneFile.py
+++ JustAnotheOneFile.py
@@ -2,4 +2,4 @@
 print('Hello Git!\nHello GitHub!')
 print("And now let's start a commiting process!")
 print("And a 2nd commit?")
-print("And what if I was nothing - This is a call to all non-believers")
+print("That's just a for testing a branch diff")
